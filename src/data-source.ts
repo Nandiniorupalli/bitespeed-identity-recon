81b6f4f0bc3dab17ecfe05db3c30fc3f1ed61192
Orupalli Nandini <nandiniorupalli@gmail.com>
2025-05-25 15:41:38 +0530

    Fix SQLite database path for deployment

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,9 +1,12 @@
 import { DataSource } from "typeorm";
 import { Contact } from "./entity/Contact";
+import path from "path";
+
+const dbPath = path.join(__dirname, "../contacts.sqlite");
 
 export const AppDataSource = new DataSource({
   type: "sqlite",
-  database: "contacts.sqlite",
+  database: dbPath,
   synchronize: true,
   logging: false,
   entities: [Contact],
